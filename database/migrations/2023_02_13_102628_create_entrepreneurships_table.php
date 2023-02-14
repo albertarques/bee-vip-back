7b81698473e471aac5397fbaa28f6507c406e002
Albert Arques <albertarques@gmail.com>
2023-02-14 12:35:23 +0100

    HotFix of Database tables and relations

---
+++ database/migrations/2023_02_13_102628_create_entrepreneurships_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('entrepreneurships', function(Blueprint $table){
+            $table->id();
+            $table->foreignId('user_id')->references('id')->on('users');
+            $table->string('title');
+            $table->string('logo')->nullable();
+            $table->string('image')->nullable();
+            $table->longText('description');
+            $table->foreignId('category_id')->references('id')->on('categories');
+            $table->string('phone');
+            // $table->string('tags');
+            // $table->string('company');
+            // $table->string('location');
+            $table->string('email');
+            // $table->string('website');
+            $table->integer('avg_score');
+            $table->char('payment_1');
+            $table->char('payment_2');
+            $table->char('payment_3');
+            $table->integer('stock');
+            $table->boolean('availability');
+            $table->timestamps();
+            });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('entrepreneurships');
+    }
+};
