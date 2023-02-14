8076fa17260883aa104e3e8099b9f95f2a225803
Albert Arques <albertarques@gmail.com>
2023-02-14 09:20:27 +0100

    Refactored EntrepreneurshipController

--- app/Http/Controllers/EntrepreneurshipController.php
+++ app/Http/Controllers/EntrepreneurshipController.php
@@ -2,84 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Entrepreneurship;
 use Illuminate\Http\Request;
 
 class EntrepreneurshipController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Entrepreneurship  $entrepreneurship
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Entrepreneurship $entrepreneurship)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Entrepreneurship  $entrepreneurship
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Entrepreneurship $entrepreneurship)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Entrepreneurship  $entrepreneurship
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Entrepreneurship $entrepreneurship)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Entrepreneurship  $entrepreneurship
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Entrepreneurship $entrepreneurship)
-    {
-        //
-    }
+    //
 }
