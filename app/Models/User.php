adc27196cd2a8053b400e5900142e418fdc2593d
Albert Arques <albertarques@gmail.com>
2023-02-15 12:52:56 +0100

    JWT develop in User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,8 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -19,8 +20,8 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'username',
-        'name',
-        'surname',
+        // 'name',
+        // 'surname',
         'email',
         'password',
         'phone',
@@ -47,8 +48,24 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    // Databse Relationships
     public function entrepreneurships(){
         return $this->hasMany(Entrepreneurship::class);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 
 }
