71171014902fd4c3de8e68eeb80a7a7a250d36c8
Francisco Aguirr <fran.uru.aguirre@gmail.com>
2023-02-16 11:50:08 +0100

    created model factory, migration and seeder of paymentmethod

---
+++ app/Models/PaymentMethod.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentMethod extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'card_name',
+        'card_number',
+        'expire_date',
+        'brand',
+        'type',
+    ];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\PaymentMethod;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -45,6 +47,9 @@ class DatabaseSeeder extends Seeder
             OrderDetailSeeder::class
         ]);
 
+        $this->call([
+            PaymentMethod::class
+        ]);
 
 
     }
