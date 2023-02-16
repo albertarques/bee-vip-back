31be33951f4aa308ac42c1f5785248623e4f2259
Guillem <guillemmj33@gmail.com>
2023-02-16 15:45:00 +0100

    permissions and roles startpoint

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,48 +9,50 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+  /**
+   * Seed the application's database.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+    // \App\Models\User::factory()->create([
+    //     'name' => 'Test User',
+    //     'email' => 'test@example.com',
+    // ]);
 
-        $this->call([
-            UserSeeder::class
-        ]);
+    $this->call([
+      UserSeeder::class
+    ]);
 
-        $this->call([
-            CategorySeeder::class
-        ]);
+    $this->call([
+      CategorySeeder::class
+    ]);
 
-        $this->call([
-            EntrepreneurshipSeeder::class
-        ]);
+    $this->call([
+      EntrepreneurshipSeeder::class
+    ]);
 
-        $this->call([
-            CommentsSeeder::class
-        ]);
+    $this->call([
+      CommentsSeeder::class
+    ]);
 
-        $this->call([
-            OrderSeeder::class
-        ]);
+    $this->call([
+      OrderSeeder::class
+    ]);
 
-        $this->call([
-            OrderDetailSeeder::class
-        ]);
+    $this->call([
+      OrderDetailSeeder::class
+    ]);
 
-        $this->call([
-            PaymentMethodsSeeder::class
-        ]);
+    $this->call([
+      PaymentMethodsSeeder::class
+    ]);
 
-
-    }
+    $this->call([
+      RoleAndPermissionSeeder::class,
+    ]);
+  }
 }
---
+++ database/seeders/RoleAndPermissionSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RoleAndPermissionSeeder extends Seeder
+{
+  public function run()
+  {
+    Permission::create(['name' => 'approve-admin-users']);
+    Permission::create(['name' => 'delete-admin-users']);
+    Permission::create(['name' => 'create-users']);
+    Permission::create(['name' => 'edit-users']);
+    Permission::create(['name' => 'delete-users']);
+    Permission::create(['name' => 'ban-users']);
+
+    Permission::create(['name' => 'create-entrepreneurships']);
+    Permission::create(['name' => 'approve-entrepreneurships']);
+    Permission::create(['name' => 'edit-entrepreneurships']);
+    Permission::create(['name' => 'delete-entrepreneurships']);
+
+    $superAdminRole = Role::create(['name' => 'SuperAdmin']);
+    $adminRole = Role::create(['name' => 'Admin']);
+    $userRole = Role::create(['name' => 'User']);
+
+    $superAdminRole->givePermissionTo([
+      'approve-admin-users',
+      'delete-admin-users',
+      'edit-users',
+      'delete-users',
+      'ban-users',
+      'approve-entrepreneurship',
+      'delete-entrepreneurship',
+    ]);
+
+    $adminRole->givePermissionTo([
+      'create-entrepreneurships',
+      'edit-entrepreneurships',
+      'delete-entrepreneurships',
+    ]);
+
+    $userRole->givePermissionTo([
+      'create-entrepreneurships',
+      'edit-entrepreneurships',
+      'delete-entrepreneurships',
+    ]);
+  }
+}
