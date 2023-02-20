e61847334e14d9569ec596fdf719ad75c6a9dff1
Albert Arques <albertarques@gmail.com>
2023-02-20 13:10:03 +0100

    Added Entrepreneurships endpoints to API

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\TodoController;
+use App\Http\Controllers\CategoriesController;
+use App\Http\Controllers\EntrepreneurshipsController;
 
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'App\Http\Controllers\AuthController@login');
@@ -14,10 +15,15 @@ Route::controller(AuthController::class)->group(function () {
 
 });
 
-// Route::controller(TodoController::class)->group(function () {
-//     Route::get('todos', 'index');
-//     Route::post('todo', 'store');
-//     Route::get('todo/{id}', 'show');
-//     Route::put('todo/{id}', 'update');
-//     Route::delete('todo/{id}', 'destroy');
-// });
+Route::controller(EntrepreneurshipsController::class)->group(function () {
+
+    // Emprendimientos
+    Route::get('entrepreneurships', [EntrepreneurshipsController::class, 'index']);
+    Route::post('entrepreneurship', [EntrepreneurshipsController::class, 'store'])->middleware('auth.entrepreneurships');
+    Route::get('entrepreneurship/{id}', [EntrepreneurshipsController::class, 'show']);
+    Route::put('entrepreneurship/{id}', [EntrepreneurshipsController::class, 'update'])->middleware('auth.entrepreneurships');
+    Route::delete('entrepreneurship/{id}', [EntrepreneurshipsController::class, 'destroy'])->middleware('auth.entrepreneurships');
+
+
+});
+
