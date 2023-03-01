32646dedf27ce0ce3aa4f910ddde27ef65eaa049
Albert Arques <albertarques@gmail.com>
2023-03-01 14:19:29 +0100

    Added role, permission and role or permission rules

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,7 +55,10 @@ class Kernel extends HttpKernel
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.jwt' => \App\Http\Middleware\JwtMiddleware::class,
-        'role' => \App\Http\Middleware\CheckRole::class,
+        // 'role' => \App\Http\Middleware\CheckRole::class,
+        'role' => \Spatie\Permission\Middlewares\RoleMiddleware::class,
+        'permission' => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
+        'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
