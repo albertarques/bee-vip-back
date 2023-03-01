417d9cffd70f539d2f2c92da82ac7ea45c720c84
Albert Arques <albertarques@gmail.com>
2023-03-01 14:20:02 +0100

    Commented state value

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,7 +25,7 @@ class User extends Authenticatable implements JWTSubject
         'email',
         'password',
         'phone',
-        'state',
+        // 'state',
         'created_at',
         'updated_at'
     ];
