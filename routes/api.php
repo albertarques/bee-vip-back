5bbfa7395b4bafabaf7dfce20c72ef694a09aad5
Albert Arques <albertarques@gmail.com>
2023-03-02 14:20:39 +0100

    Added Public Routes to Api

--- routes/api.php
+++ routes/api.php
@@ -10,13 +10,27 @@ use App\Http\Controllers\UsersController;
 use App\Http\Controllers\OrdersController;
 use App\Http\Controllers\OrderDetailsController;
 use App\Http\Controllers\PaymentMethodsController;
-use App\Http\Controllers\RolesController;
-use App\Http\Controllers\RoleAsignmentsController;
 
-// Rutas públicas
-// TODO : RUTAS PUBLICAS
+// ** Rutas públicas ******************************************************
+    // Entrepreneurships
+    Route::controller(EntrepreneurshipsController::class)->group(function () {
+        Route::get('entrepreneurships', 'index');
+        Route::get('entrepreneurship/{id}', 'show');
+    });
 
-// Rutas con Autenticación.
+    // Usuarios
+    Route::controller(UsersController::class)->group(function () {
+        Route::get('user/{id}', 'show');
+    });
+
+    // Categorías
+    Route::controller(CategoriesController::class)->group(function () {
+        Route::get('categories', 'index');
+        Route::get('category/{id}', 'show');
+    });
+
+
+// ** Rutas con Autenticación ******************************************************
 Route::controller(AuthController::class)->group(function () {
     Route::post('login', 'App\Http\Controllers\AuthController@login');
     Route::post('register', 'App\Http\Controllers\AuthController@register');
@@ -31,52 +45,52 @@ Route::controller(AuthController::class)->group(function () {
 // Rutas con Autorización y Permisos
 Route::group(['middleware' => 'auth.jwt'], function () {
     Route::controller(UsersController::class)->group(function () {
-        Route::post('/user/update', 'update')->middleware('can:update-user-profile');
-        Route::post('/user/delete', 'destroy')->middleware('can:delete-user-profile');
+        Route::put('/user/{id}/update', 'update')->middleware('can:update-user-profile');
+        Route::delete('/user/{id}/delete', 'destroy')->middleware('can:delete-user-profile');
     });
 
     Route::controller(PaymentMethodsController::class)->group(function () {
         Route::post('/paymentmethod/create', 'store')->middleware('can:create-payment-method');
-        Route::post('/paymentmethod/view', 'view')->middleware('can:view-payment-method');
-        Route::post('/paymentmethod/update', 'update')->middleware('can:update-payment-method');
-        Route::post('/paymentmethod/delete', 'destroy')->middleware('can:delete-payment-method');
+        Route::get('/paymentmethod/{id}/view', 'view')->middleware('can:view-payment-method');
+        Route::patch('/paymentmethod/{id}/update', 'update')->middleware('can:update-payment-method');
+        Route::delete('/paymentmethod/{id}/delete', 'destroy')->middleware('can:delete-payment-method');
     });
 
     Route::controller(OrdersController::class)->group(function () {
         Route::post('/order/create', 'store')->middleware('can:create-order');
-        Route::post('/order/view', 'view')->middleware('can:view-order');
+        Route::get('/order/{id}/view', 'view')->middleware('can:view-order');
     });
 
     Route::controller(OrderDetailsController::class)->group(function () {
         Route::post('/orderdetail/create', 'store')->middleware('can:create-order-detail');
-        Route::post('/orderdetail/view', 'view')->middleware('can:view-order-detail');
+        Route::get('/orderdetail/{id}/view', 'view')->middleware('can:view-order-detail');
     });
 
     Route::controller(EntrepreneurshipsController::class)->group(function () {
         Route::post('/entrepreneurship/create', 'store')->middleware('can:create-entrepreneurship');
-        Route::post('/entrepreneurship/update', 'update')->middleware('can:update-entrepreneurship');
-        Route::post('/entrepreneurship/delete', 'destroy')->middleware('can:delete-entrepreneurship');
+        Route::patch('/entrepreneurship/{id}/update', 'update')->middleware('can:update-entrepreneurship');
+        Route::delete('/entrepreneurship/{id}/delete', 'destroy')->middleware('can:delete-entrepreneurship');
+        Route::get('/entrepreneurships_pending', 'store')->middleware('can:create-entrepreneurship');
+
     });
 
     Route::controller(CommentsController::class)->group(function () {
         Route::post('/comment/create', 'store')->middleware('can:create-comment');
-        Route::post('/comment/update', 'update')->middleware('can:update-comment');
-        Route::post('/comment/delete', 'destroy')->middleware('can:delete-comment');
+        Route::patch('/comment/{id}/update', 'update')->middleware('can:update-comment');
+        Route::delete('/comment/{id}/delete', 'destroy')->middleware('can:delete-comment');
     });
 
     Route::controller(CommentsController::class)->group(function () {
         Route::post('/comment/create', 'store')->middleware('can:create-comment');
-        Route::post('/comment/update', 'update')->middleware('can:update-comment');
-        Route::post('/comment/delete', 'destroy')->middleware('can:delete-comment');
+        Route::patch('/comment/{id}/update', 'update')->middleware('can:update-comment');
+        Route::delete('/comment/{id}/delete', 'destroy')->middleware('can:delete-comment');
     });
 
 
 
 });
 
-
 // Usuarios
-
 // Route::controller(UsersController::class)->group(function () {
     // Route::get('users', [UsersController::class, 'index']);
     // Devuelve un usuario y sus emprendimientos.
@@ -116,52 +130,4 @@ Route::group(['middleware' => 'auth.jwt'], function () {
 //     Route::get('user/{id}', 'show');
 //     Route::put('user/{id}', 'update');
 //     Route::delete('user/{id}', 'destroy');
-// });
-
-// Ordenes
-// Route::controller(OrdersController::class)->group(function () {
-//     Route::post('order', 'store');
-//     Route::get('order/{id}', 'show');
-    // Route::put('order/{id}', 'update')->middleware('auth.orders');
-    // Route::delete('order/{id}', 'destroy'])->middleware('auth.orders');
-// });
-
-// Detalles de la orden
-// Route::controller(OrderDetailsController::class)->group(function () {
-//     Route::post('order_detail', 'store');
-//     Route::get('order_detail/{id}', 'show');
-//     Route::put('order_detail/{id}', 'update');
-//     // Route::delete('order_detail/{id}', 'destroy'])->middleware('auth.order_details');
-// });
-
-// Métodos de pago
-// Route::controller(PaymentMethodsController::class)->group(function () {
-//     // TODO: Devolver los métodos del pago del usuario.
-//     Route::get('payment_methods','index');
-//     // TODO: Añadir método de pago.
-//     Route::post('payment_method','store');
-//     // TODO: Ver método de pago.
-//     Route::get('payment_method/{id}','show');
-//     // TODO: Actualizar método de pago
-//     Route::put('payment_method/{id}','update');
-//     // TODO: Eliminar.
-//     Route::delete('payment_method/{id}','destroy');
-// });
-
-// Roles
-// Route::controller(RolesController::class)->group(function () {
-//     Route::get('roles', 'index');
-//     Route::post('role', 'store');
-//     Route::get('role/{id}', 'show');
-//     Route::put('role/{id}', 'update');
-//     Route::delete('role/{id}', 'destroy');
-// });
-
-// Asignación de roles
-// Route::controller(RoleAsignmentsController::class)->group(function () {
-//     Route::get('roleAssignments', 'index');
-//     Route::post('roleAssignment', 'store');
-//     Route::get('roleAssignment/{id}', 'show');
-//     Route::put('roleAssignment/{id}', 'update');
-//     Route::delete('roleAssignment/{id}', 'destroy');
 // });
