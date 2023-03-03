70a37898c69eef9870e1d37e78a83112aa8f7c60
Albert Arques <albertarques@gmail.com>
2023-03-03 17:04:51 +0100

    Added view pending entrepreneurships route

--- routes/api.php
+++ routes/api.php
@@ -16,22 +16,22 @@ use App\Http\Controllers\RoleAsignmentsController;
 use App\Http\Controllers\EntrepreneurshipsController;
 
 // ** Rutas públicas ******************************************************
-    // Entrepreneurships
-    Route::controller(EntrepreneurshipsController::class)->group(function () {
-        Route::get('entrepreneurships', 'availableIndex');
-        Route::get('entrepreneurship/{id}', 'show');
-    });
+// Entrepreneurships
+Route::controller(EntrepreneurshipsController::class)->group(function () {
+    Route::get('entrepreneurships', 'availableIndex');
+    Route::get('entrepreneurship/{id}', 'show');
+});
 
-    // Usuarios
-    Route::controller(UsersController::class)->group(function () {
-        Route::get('user/{id}', 'show');
-    });
+// Usuarios
+Route::controller(UsersController::class)->group(function () {
+    Route::get('user/{id}', 'show');
+});
 
-    // Categorías
-    Route::controller(CategoriesController::class)->group(function () {
-        Route::get('categories', 'index');
-        Route::get('category/{id}', 'show');
-    });
+// Categorías
+Route::controller(CategoriesController::class)->group(function () {
+    Route::get('categories', 'index');
+    Route::get('category/{id}', 'show');
+});
 
 
 // ** Rutas con Autenticación ******************************************************
@@ -48,6 +48,9 @@ Route::controller(AuthController::class)->group(function () {
 
 // Rutas con Autorización y Permisos
 Route::group(['middleware' => 'auth.jwt'], function () {
+
+    Route::post('image',[ImageController::class, 'imageStore']);
+
     Route::controller(UsersController::class)->group(function () {
         Route::patch('/user/{id}/update', 'update')->middleware('can:update-user-profile');
         Route::delete('/user/{id}/delete', 'destroy')->middleware('can:delete-user-profile');
@@ -74,7 +77,7 @@ Route::group(['middleware' => 'auth.jwt'], function () {
         Route::post('/entrepreneurship/create', 'store')->middleware('can:create-entrepreneurship');
         Route::patch('/entrepreneurship/{id}/update', 'update')->middleware('can:update-entrepreneurship');
         Route::delete('/entrepreneurship/{id}/delete', 'destroy')->middleware('can:delete-entrepreneurship');
-        Route::get('/entrepreneurships_pending', 'store')->middleware('can:create-entrepreneurship');
+        // Route::get('/entrepreneurships_pending', 'store')->middleware('can:create-entrepreneurship');
         Route::get('/entrepreneurships/pending', 'pendingIndex')->middleware('can:view-pending-entrepreneurships');
 
     });
@@ -85,41 +88,3 @@ Route::group(['middleware' => 'auth.jwt'], function () {
         // Route::delete('/comment/{id}/delete', 'destroy')->middleware('can:delete-comment');
     });
 });
-
-Route::post('image',[ImageController::class, 'imageStore']);
-
-// Usuarios
-// Route::controller(UsersController::class)->group(function () {
-    // Route::get('users', [UsersController::class, 'index']);
-    // Devuelve un usuario y sus emprendimientos.
-    // Route::get('user', 'show');
-// });
-
-//Categorías
-Route::controller(CategoriesController::class)->group(function () {
-    Route::get('categories', 'index');
-    Route::post('category', 'store');
-    Route::get('category/{id}', 'show');
-    Route::put('category/{id}', 'update');
-    // Route::delete('category/{id}', 'destroy')->middleware('auth.entrepreneurships');
-});
-
-// Emprendimientos
-Route::controller(EntrepreneurshipsController::class)->group(function () {
-    Route::get('entrepreneurships', 'approvedIndex');
-    Route::get('entrepreneurships_pending', 'pendingIndex');
-    Route::post('entrepreneurship', 'store')->middleware('auth.');
-    Route::get('entrepreneurship/{id}', 'show');
-    Route::put('entrepreneurship/{id}', 'update');
-    Route::delete('entrepreneurship/{id}', 'destroy');
-});
-
-// Comentarios
-// Route::controller(CommentsController::class)->group(function () {
-//     Route::post('comment', 'store');
-//     Route::get('comment/{id}', 'show');
-//     Route::put('comment/{id}', 'update');
-//     Route::delete('comment/{id}', 'destroy');
-// });
-
-// });
