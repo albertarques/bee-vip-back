baead2aa889a267b5183f0f72614ae631c785883
Albert Arques <albertarques@gmail.com>
2023-03-06 09:23:27 +0100

    Deleted some commented lines

--- app/Permissions/Permission.php
+++ app/Permissions/Permission.php
@@ -19,25 +19,15 @@ class Permission {
     // Orders
     public const CAN_CREATE_ORDER = "create-order";
     public const CAN_VIEW_ORDER = "view-order";
-    // ¿? public const CAN_UPDATE_ORDER = "update-order";
-    // ¿? public const CAN_DELETE_ORDER = "delete-order";
 
     // Order details
     public const CAN_CREATE_ORDER_DETAIL = "create-order-detail";
     public const CAN_VIEW_ORDER_DETAIL = "view-order-detail";
-    // ¿? public const CAN_UPDATE_ORDER_DETAIL = "update-order-detail";
-    // ¿? public const CAN_DELETE_ORDER_DETAIL = "delete-order-detail";
 
     // Comment Entrepreneurships
     public const CAN_CREATE_COMMENT = "create-comment";
-    // ¿? public const CAN_VIEW_COMMENT = "view-comment";
-    // TODO: public const CAN_UPDATE_COMMENT = "update-comment";
-    // TODO: public const CAN_DELETE_COMMENT = "delete-comment";
 
     // *************** Admin Permissions ************************************
-    // User Profile
-    // public const CAN_VIEW_USERPROFILE = "view-user-profile";
-
     // Entrepreneurships
     public const CAN_CREATE_ENTREPRENEURSHIP = "create-entrepreneurship";
     public const CAN_UPDATE_ENTREPRENEURSHIP = "update-entrepreneurship";
@@ -45,15 +35,13 @@ class Permission {
     public const CAN_VIEW_PENDING_ENTREPRENEURSHIPS = "view-pending-entrepreneurships";
     public const CAN_VIEW_ENTREPRENEURSHIPS = "view-entrepreneurships";
 
-    // *************** Superadmin Permissions ************************************
+    // *************** Superadmin Permissions ********************************
     // Users
     public const CAN_DELETE_USER = "delete-user";
-    public const CAN_UPGRADE_USER_ROLE = "upgrade-user-role";
+    public const CAN_UPDATE_USER_ROLE = "update-user-role";
 
     // Roles
-    public const CAN_UPDATE_USER_ROLE = "update-user-role";
     public const CAN_SHOW_USER_ROLE = "show-user-role";
-
 
     // Entrepreneurships
     public const CAN_APPROVE_ENTREPRENEURSHIPS = "approve-entrepreneurship";
