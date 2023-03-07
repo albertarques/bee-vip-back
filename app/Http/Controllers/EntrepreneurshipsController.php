4ec618d306c0d3c179c3dc0f84712e19b2fe87a3
Albert Arques <albertarques@gmail.com>
2023-03-07 20:15:52 +0100

    Refactored code & validations

--- app/Http/Controllers/EntrepreneurshipsController.php
+++ app/Http/Controllers/EntrepreneurshipsController.php
@@ -7,7 +7,7 @@ use App\Models\Entrepreneurship;
 use App\Models\Comment;
 use App\Models\User;
 use App\Models\Category;
-
+use App\Models\InspectionState;
 
 class EntrepreneurshipsController extends Controller
 {
@@ -63,47 +63,42 @@ class EntrepreneurshipsController extends Controller
         ]);
     }
 
-
     public function store(Request $request){
         $request->validate([
-            'user_id' => 'required|string|max:255',
-            'title' => 'required|string|max:255',
-            'logo' => 'required|string|max:255',
-            'title' => 'required|string|max:255',
-            'product_img' => 'required|string|max:255',
-            'description' => 'required|string|max:500',
-            'price' => 'required|float',
-            'category_id' => 'required|string|max:255',
-            'avg_score' => 'required|float',
-            'cash_payment' => 'required|boolean',
-            'card_payment' => 'required|boolean',
-            'bizum_payment' => 'required|boolean',
-            'stock' => 'required|integer|max:500',
-            'availability_state' => 'required|integer|min:1|max:2',
-            'phone' => 'required|string|digits_between:9,15',
-            'email' => 'required|integer|max:255',
-            'location' => 'required|integer|max:255',
-            'inspection_state' => 'required|integer|min:1|max:3',
+            // 'user_id' => 'required|integer|exists:users,id',
+            // 'title' => 'required|string|max:255',
+            // 'logo' => 'nullable|url',
+            // 'product_img' => 'nullable|url',
+            // 'description' => 'required|string|max:500',
+            // 'price' => 'required'|'numeric|regex:/^\d+(\.\d{1,2})?$/',
+            // 'category_id' => 'required|exists:categories,id',
+            // 'avg_score' => 'nullable|numeric|regex:/^\d+(\.\d{1,2})?$/',
+            // 'cash_payment' => 'required|boolean',
+            // 'card_payment' => 'required|boolean',
+            // 'bizum_payment' => 'required|boolean',
+            // 'stock' => 'required|integer|max:500',
+            // 'availability_state' => 'required|integer|exists:availability_states,id|between:1, 2',
+            // 'phone' => 'required|string|digits_between:9,15',
+            // 'email' => 'required|email',
+            // 'location' => 'required|string|max:255',
+            // 'inspection_state' => 'required|integer|exists:inspection_states,id|between:1, 3',
         ]);
 
-        // dd($request);
         $entrepreneurship = Entrepreneurship::create([
-
-            'user_id' => $request->user_id,
+            // 'user_id' => $request->user_id,
             'title' => $request->title,
-            'name' => $request->name,
             'logo' => $request->logo,
             'product_img' => $request->product_img,
             'description' => $request->description,
             'price' => $request->price,
             'category_id' => $request->category_id,
-            'avg_score' => $request->avg_score,
+            // 'avg_score' => $request->avg_score,
             'cash_payment' => $request->cash_payment,
             'card_payment' => $request->card_payment,
             'bizum_payment' => $request->bizum_payment,
             'stock' => $request->stock,
             'availability_state' => 1,
-            'phone_number' => $request->phone_number,
+            'phone' => $request->phone,
             'email' => $request->email,
             'location' => $request->location,
             'inspection_state' => 1,
@@ -138,46 +133,82 @@ class EntrepreneurshipsController extends Controller
 
     public function update(Request $request, $id){
         $request->validate([
-            'user_id' => 'required|string|max:255',
-            'title' => 'required|string|max:255',
-            'logo' => 'nullable|string|max:255',
-            'product_img' => 'nullable|string|max:255',
-            'description' => 'required|string|max:500',
-            'price' => 'required|float',
-            'category_id' => 'required|string|max:255',
-            'avg_score' => 'nullable|float',
-            'cash_payment' => 'required|boolean',
-            'card_payment' => 'required|boolean',
-            'bizum_payment' => 'required|boolean',
-            'stock' => 'nullable|integer|max:500',
-            'availability_state' => 'required|integer|min:1|max:2',
-            'phone' => 'required|string|digits_between:9,15',
-            'email' => 'required|integer|max:255',
-            'location' => 'required|integer|max:255',
-            'inspection_state' => 'requiered|integer|min:1|max:3',
+          'user_id' => 'required|integer|exists:users,id',
+          'title' => 'required|string|max:255',
+          'logo' => 'nullable|url',
+          'product_img' => 'nullable|url',
+          'description' => 'required|string|max:500',
+          'price' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
+          'category_id' => 'required|exists:categories,id',
+          'avg_score' => 'nullable|numeric|regex:/^\d+(\.\d{1,2})?$/',
+          'cash_payment' => 'required|boolean',
+          'card_payment' => 'required|boolean',
+          'bizum_payment' => 'required|boolean',
+          'stock' => 'required|integer|max:500',
+          'availability_state' => 'required|integer|exists:availability_states,id|between:1, 2',
+          'phone' => 'required|string|digits_between:9,15',
+          'email' => 'required|email',
+          'location' => 'required|string|max:255',
+          'inspection_state' => 'required|integer|exists:inspection_states,id|between:1, 3',
         ]);
 
         $entrepreneurship = Entrepreneurship::find($id);
+        $entrepreneurship->user_id = $entrepreneurship->user_id;
         $entrepreneurship->title = $request->title;
+        $entrepreneurship->logo = $request->logo;
+        $entrepreneurship->product_img = $request->product_img;
         $entrepreneurship->description = $request->description;
+        $entrepreneurship->price = $request->price;
+        $entrepreneurship->category_id = $request->category_id;
+        $entrepreneurship->avg_score = $entrepreneurship->avg_score;
+        $entrepreneurship->cash_payment = $request->cash_payment;
+        $entrepreneurship->card_payment = $request->card_payment;
+        $entrepreneurship->bizum_payment = $request->bizum_payment;
+        $entrepreneurship->stock = $request->stock;
+        $entrepreneurship->availability_state = $request->availability_state;
+        $entrepreneurship->phone = $request->phone;
+        $entrepreneurship->email = $request->email;
+        $entrepreneurship->location = $request->location;
+        $entrepreneurship->inspection_state = $entrepreneurship->inspection_state;
         $entrepreneurship->save();
 
         return response()->json([
+            'code' => 200,
             'status' => 'success',
-            'message' => 'entrepreneurship updated successfully',
+            'message' => 'Entrepreneurship updated successfully',
             'entrepreneurship' => $entrepreneurship,
         ]);
     }
 
+    public function updateInspectionState(Request $request, $id){
+
+      $request->validate([
+        'inspection_state' => 'required|integer|min:1|max:3',
+      ]);
+
+      $entrepreneurship = Entrepreneurship::find($id);
+      $newState = $request->inspection_state;
+
+      $entrepreneurship->inspection_state = $newState;
+      $entrepreneurship->save();
+
+      return response()->json([
+        'code' => 200,
+        'message' => 'Entrepreneurship inspection state updated successfully',
+        'entrepreneurship' => $entrepreneurship,
+      ]);
+    }
+
     public function destroy($id)
     {
-        $entrepreneurship = Entrepreneurship::find($id);
-        $entrepreneurship->delete();
+      $entrepreneurship = Entrepreneurship::find($id);
+      $entrepreneurship->delete();
 
-        return response()->json([
-            'status' => 'success',
-            'message' => 'entrepreneurship deleted successfully',
-            'entrepreneurship' => $entrepreneurship,
-        ]);
+      return response()->json([
+        'code' => 200,
+        'status' => 'success',
+        'message' => 'entrepreneurship deleted successfully',
+        'entrepreneurship' => $entrepreneurship,
+      ]);
     }
 }
