8d6cc51c7154554688f142e38c8548d6cb722a2c
AlejoR27 <alejandrobr27@hotmail.com>
2023-03-08 22:06:56 +0100

    Create availabilityStateFactory

---
+++ database/factories/AvailabilityStateFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\AvailabilityState>
+ */
+class AvailabilityStateFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->sentence($nbWords = 1, $variableNbWords = true),
+        ];
+    }
+}
