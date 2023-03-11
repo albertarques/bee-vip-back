d4bc38df57c8aa898db8e20a39de8291e553c316
Albert Arques <albertarques@gmail.com>
2023-03-11 10:22:29 +0100

    New Entrepreneurship Controller

--- app/Http/Controllers/EntrepreneurshipsController.php
+++ app/Http/Controllers/EntrepreneurshipsController.php
@@ -2,21 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Entrepreneurship;
-use App\Models\Comment;
-use App\Models\User;
 use App\Models\Category;
-use App\Models\InspectionState;
+use App\Models\Comment;
+use App\Models\Entrepreneurship;
+use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class EntrepreneurshipsController extends Controller
 {
-    //
-    public function __construct(){
-        $this->middleware('api');
-    }
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index(){
       $entrepreneurships = Entrepreneurship::all();
 
@@ -24,265 +23,161 @@ class EntrepreneurshipsController extends Controller
         'status'=>'success',
         'entrepreneurships'=>$entrepreneurships,
       ]);
+
     }
 
+    /**
+     * Display an approved state list of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function approvedIndex(){
-        // TODO: Obtiene todos los emprendimientos aprovados y todas las categorías.
-        $entrepreneurships = Entrepreneurship::all();
-        $category = Category::all();
+      // TODO: Obtiene todos los emprendimientos aprovados y todas las categorías.
+      $entrepreneurships = Entrepreneurship::all();
+      // $category = Category::all();
 
-        return response()->json([
-            'code' => 200,
-            'status' => 'success',
-            'entrepreneurships' => [...$entrepreneurships],
-            // 'categories' => $category,
-        ]);
+      return response()->json([
+          'code' => 200,
+          'status' => 'success',
+          'entrepreneurships' => [...$entrepreneurships],
+          // 'categories' => $category,
+      ]);
     }
 
+    /**
+     * Display a pending state list of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function pendingIndex(){
-        // TODO: Obtiene todos los emprendimientos pendientes de aprovación.
-        $entrepreneurships = Entrepreneurship::all()->where('inspection_state', '=', 1);
+      // TODO: Obtiene todos los emprendimientos pendientes de aprovación.
+      $entrepreneurships = Entrepreneurship::all()->where('inspection_state', '=', 1);
 
-        return response()->json([
-            'status' => 'success',
-            'entrepreneurships' => [...$entrepreneurships],
-        ]);
+      return response()->json([
+          'status' => 'success',
+          'entrepreneurships' => [...$entrepreneurships],
+      ]);
     }
 
+    /**
+     * Display an available & approved state  list of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function availableIndex(){
-        // TODO: Obtiene todos los emprendimientos aprovados y disponibles, y todas las categorías.
-        $entrepreneurships = Entrepreneurship::all()->where('inspection_state', '=', 2)->where('availability_state', '=', 2);
-        // $category = Category::all();
+      // TODO: Obtiene todos los emprendimientos aprovados y disponibles, y todas las categorías.
+      $entrepreneurships = Entrepreneurship::all()->where('inspection_state', '=', 2)->where('availability_state', '=', 2);
+      // $category = Category::all();
 
-        return response()->json([
-            'status' => 'success',
-            'entrepreneurships' => [...$entrepreneurships],
-        ]);
+      return response()->json([
+          'status' => 'success',
+          'entrepreneurships' => [...$entrepreneurships],
+      ]);
     }
 
-    public function myEntrepreneurships(){
-      // Obtener el usuario autenticado
-      $user = auth()->user();
-
-      // Obtener los emprendimientos asociados al usuario
-      $entrepreneurships = $user->entrepreneurships;
-
-      // Retornar los emprendimientos en formato JSON
-      return response()->json($entrepreneurships);
-    }
-
-    public function store(Request $request){
-        $request->validate([
-            'title' => 'required|string|max:100',
-            'product_img' => 'image|max:2048',
-            'description' => 'required|string|max:100',
-            'price' => 'required|integer',
-
-
-            // 'user_id' => 'required|integer|exists:users,id',
-            // 'title' => 'required|string|max:255',
-            // 'logo' => 'nullable|url',
-            // 'product_img' => 'nullable|url',
-            // 'description' => 'required|string|max:500',
-            // 'price' => 'required'|'numeric|regex:/^\d+(\.\d{1,2})?$/',
-            // 'category_id' => 'required|exists:categories,id',
-            // 'avg_score' => 'nullable|numeric|regex:/^\d+(\.\d{1,2})?$/',
-            // 'cash_payment' => 'required|boolean',
-            // 'card_payment' => 'required|boolean',
-            // 'bizum_payment' => 'required|boolean',
-            // 'stock' => 'required|integer|max:500',
-            // 'availability_state' => 'required|integer|exists:availability_states,id|between:1, 2',
-            // 'phone' => 'required|string|digits_between:9,15',
-            // 'email' => 'required|email',
-            // 'location' => 'required|string|max:255',
-            // 'inspection_state' => 'required|integer|exists:inspection_states,id|between:1, 3',
-        ]);
-
-        $imagePath = $request->file('image')->store('public/images');
-        $imageUrl = url('storage/' . str_replace('public/', '', $imagePath));
-
-        $entrepreneurship = Entrepreneurship::create([
-            'user_id' => $request->user_id,
-            'title' => $request->title,
-            'logo' => $request->logo,
-            'product_img' => $request->product_img,
-            'description' => $request->description,
-            'price' => $request->price,
-            'category_id' => $request->category_id,
-            'avg_score' => $request->avg_score,
-            'cash_payment' => $request->cash_payment,
-            'card_payment' => $request->card_payment,
-            'bizum_payment' => $request->bizum_payment,
-            'stock' => $request->stock,
-            'availability_state' => 2,
-            'phone' => $request->phone,
-            'email' => $request->email,
-            'location' => $request->location,
-            'inspection_state' => 2,
-        ]);
-
-        return response()->json([
-            'code' => 200,
-            'status' => 'success',
-            'message' => 'Entrepreneurship created successfully',
-            'entrepreneurship' => $entrepreneurship,
-        ]);
-    }
-
-    public function show($id){
-        // Obtiene el emprendimiento con su categoria, sus comentarios y el usuario propietário.
-        $entrepreneurship = Entrepreneurship::find($id);
-        $comments = Comment::all()->where('entrepreneurship_id', '=', $id);
-        $user_id = $entrepreneurship->user_id;
-        $user = User::all()->where('id', '=', $user_id);
-        $category_id = $entrepreneurship->category_id;
-        $category = Category::all()->where('id', '=', $category_id);
-
-
-        return response()->json([
-            'status' => 'success',
-            'category' => $category,
-            'entrepreneurship' => $entrepreneurship,
-            'comments' => $comments,
-            'user' => $user,
-        ]);
-    }
-
-    public function updateMyEntrepreneurship(Request $request, $entrepreneurship_id){
-
-      $entrepreneurship = Entrepreneurship::find($entrepreneurship_id);
-      $user = User::find($entrepreneurship->user_id);
-
-      // $validatedData = $request->validate([
-      //   'title' => 'required|max:255',
-      //   'logo' => 'nullable|image|max:1024',
-      //   'product_img' => 'nullable|image|max:1024',
-      //   'description' => 'required',
-      //   'price' => 'required|numeric',
-      //   'category_id' => 'required|exists:categories,id',
-      //   'avg_score' => 'nullable|numeric|min:0|max:5',
-      //   'cash_payment' => 'required|boolean',
-      //   'card_payment' => 'required|boolean',
-      //   'bizum_payment' => 'required|boolean',
-      //   'stock' => 'required|integer|min:0',
-      //   'availability_state' => 'required|in:disponible,no_disponible',
-      //   'phone' => 'nullable|string|max:20',
-      //   'email' => 'nullable|email|max:255',
-      //   'location' => 'nullable|max:255',
-      //   'id' => '$entrepreneurship->id',
-      //   'user_id' => 'prohibited',
-      //   'inspection_state' => 'prohibited',
-      //   'created_at' => 'prohibited',
-      //   'updated_at' => 'prohibited'
-      // ]);
-
-
-      if (!$entrepreneurship) {
-          return response()->json(['message' => 'El emprendimiento no existe'], 404);
-      }
-
-      if ($entrepreneurship->user_id != $user->id) {
-          return response()->json(['message' => 'No tienes permiso para actualizar este emprendimiento'], 403);
-      }
-
-      // if ($request->hasFile('logo')) {
-      //     $logo = $request->file('logo');
-      //     $logoName = time() . '_' . $logo->getClientOriginalName();
-      //     $logo->move(public_path('images'), $logoName);
-      //     $validatedData['logo'] = $logoName;
-      // }
-
-      // if ($request->hasFile('product_img')) {
-      //     $productImg = $request->file('product_img');
-      //     $productImgName = time() . '_' . $productImg->getClientOriginalName();
-      //     $productImg->move(public_path('images'), $productImgName);
-      //     $validatedData['product_img'] = $productImgName;
-      // }
-
-      // $entrepreneurship->update($validatedData);
-
-      return response()->json(['message' => 'Emprendimiento actualizado correctamente'], 200);
-    }
-
-    public function update(Request $request, $id){
-    //     $request->validate([
-          // 'user_id' => 'required|integer|exists:users,id',
-          // 'title' => 'required|string|max:255',
-          // 'logo' => 'nullable|url',
-          // 'product_img' => 'nullable|url',
-          // 'description' => 'required|string|max:500',
-          // 'price' => 'required|numeric|regex:/^\d+(\.\d{1,2})?$/',
-          // 'category_id' => 'required|exists:categories,id',
-          // 'avg_score' => 'nullable|numeric|regex:/^\d+(\.\d{1,2})?$/',
-          // 'cash_payment' => 'required|boolean',
-          // 'card_payment' => 'required|boolean',
-          // 'bizum_payment' => 'required|boolean',
-          // 'stock' => 'required|integer|max:500',
-          // 'availability_state' => 'required|integer|exists:availability_states,id|between:1, 2',
-          // 'phone' => 'required|string|digits_between:9,15',
-          // 'email' => 'required|email',
-          // 'location' => 'required|string|max:255',
-          // 'inspection_state' => 'required|integer|exists:inspection_states,id|between:1, 3',
-        // ]);
-
-        $entrepreneurship = Entrepreneurship::find($id);
-        // $entrepreneurship->id = $entrepreneurship->id;
-        // $entrepreneurship->user_id = $entrepreneurship->user_id;
-        // $entrepreneurship->title = $request->title;
-        // $entrepreneurship->logo = $request->logo;
-        // $entrepreneurship->product_img = $request->product_img;
-        // $entrepreneurship->description = $request->description;
-        // $entrepreneurship->price = $request->price;
-        // $entrepreneurship->category_id = $request->category_id;
-        // $entrepreneurship->avg_score = $entrepreneurship->avg_score;
-        // $entrepreneurship->cash_payment = $request->cash_payment;
-        // $entrepreneurship->card_payment = $request->card_payment;
-        // $entrepreneurship->bizum_payment = $request->bizum_payment;
-        // $entrepreneurship->stock = $request->stock;
-        // $entrepreneurship->availability_state = $request->availability_state;
-        // $entrepreneurship->phone = $request->phone;
-        // $entrepreneurship->email = $request->email;
-        // $entrepreneurship->location = $request->location;
-        // $entrepreneurship->inspection_state = $entrepreneurship->inspection_state;
-        // $entrepreneurship->save();
-
-        return response()->json([
-            'code' => 200,
-            'status' => 'success',
-            'message' => 'Entrepreneurship updated successfully',
-            'entrepreneurship' => $entrepreneurship,
-        ]);
-    }
-
-    public function updateInspectionState(Request $request, $id){
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Request $request){
 
       $request->validate([
-        'inspection_state' => 'required|integer|min:1|max:3',
+        'user_id' => 'required|exists:users,id',
+        'title' => 'required|max:255',
+        'description' => 'required|max:1000',
+        'price' => 'required|numeric|min:0',
+        'category_id' => 'required|exists:categories,id',
+        'cash_payment' => 'required|boolean',
+        'card_payment' => 'required|boolean',
+        'bizum_payment' => 'required|boolean',
+        'stock' => 'nullable|integer|min:0',
+        'availability_state' => 'required|exists:availability_states,id',
+        // 'inspection_state' => 'required|exists:inspection_states,id',
+        'phone' => 'nullable',
+        'email' => 'nullable|email',
+        'location' => 'required'
       ]);
 
-      $entrepreneurship = Entrepreneurship::find($id);
-      $newState = $request->inspection_state;
-
-      $entrepreneurship->inspection_state = $newState;
+      $entrepreneurship = new Entrepreneurship;
+      $entrepreneurship->user_id = $request->user_id;
+      $entrepreneurship->title = $request->title;
+      $entrepreneurship->description = $request->description;
+      $entrepreneurship->price = $request->price;
+      $entrepreneurship->category_id = $request->category_id;
+      $entrepreneurship->cash_payment = $request->cash_payment;
+      $entrepreneurship->card_payment = $request->card_payment;
+      $entrepreneurship->bizum_payment = $request->bizum_payment;
+      $entrepreneurship->stock = $request->stock;
+      $entrepreneurship->availability_state = 1;
+      $entrepreneurship->inspection_state = 1;
+      $entrepreneurship->phone = $request->phone;
+      $entrepreneurship->email = $request->email;
+      $entrepreneurship->location = $request->location;
       $entrepreneurship->save();
 
       return response()->json([
-        'code' => 200,
-        'message' => 'Entrepreneurship inspection state updated successfully',
-        'entrepreneurship' => $entrepreneurship,
+        'status' => 'success',
+        'message' => 'Entrepreneurship created successfully',
+        'entreprenenurship' => $entrepreneurship
       ]);
     }
 
-    public function destroy($id)
-    {
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id){
+      // Obtiene el emprendimiento con su categoria, sus comentarios y el usuario propietário.
       $entrepreneurship = Entrepreneurship::find($id);
-      $entrepreneurship->delete();
+      $comments = Comment::all()->where('entrepreneurship_id', '=', $id);
+      $user_id = $entrepreneurship->user_id;
+      $user = User::find($user_id);
+      $category_id = $entrepreneurship->category_id;
+      $category = Category::all()->where('id', '=', $category_id);
 
       return response()->json([
-        'code' => 200,
-        'status' => 'success',
-        'message' => 'entrepreneurship deleted successfully',
-        'entrepreneurship' => $entrepreneurship,
+          'status' => 'success',
+          'category' => $category,
+          'entrepreneurship' => $entrepreneurship,
+          'comments' => $comments,
+          'user' => $user,
       ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
