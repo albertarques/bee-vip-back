1d58aa3477a50ec3212ed50f4c3def3ee94ea179
Albert Arques <albertarques@gmail.com>
2023-03-21 18:14:28 +0100

    Added default image to User Factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -20,7 +20,7 @@ class UserFactory extends Factory
     {
         return [
             'username' => fake()->userName(),
-            'picture' =>fake()->image(),
+            'picture' => 'images/default/default-user.png',
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
