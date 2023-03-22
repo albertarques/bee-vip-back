3335a21f03705151e3d2b2099ab9eff5970ea554
AlejoR27 <alejandrobr27@hotmail.com>
2023-03-22 14:14:18 +0100

    Descommented all test

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -12,129 +12,128 @@ use Illuminate\Support\Facades\Artisan;
 class CommentTest extends TestCase
 {
 
-    // public function setUp(): void
-    // {
-    //     parent::setUp();
-    //     Artisan::call('migrate:fresh --seed --env=testing');
-    // }
+    public function setUp(): void
+    {
+        parent::setUp();
+        Artisan::call('migrate:fresh --seed --env=testing');
+    }
     /** @test */
     // comment/create/{entrepreneurship_id}
-    // public function ok_is_returned_if_user_can_create_comment()
-    // {
-    //     // Crear un usuario utilizando el Factory
-    //     $user = User::factory()->create();
-    //     $user->assignRole('user');
+    public function ok_is_returned_if_user_can_create_comment()
+    {
+        // Crear un usuario utilizando el Factory
+        $user = User::factory()->create();
+        $user->assignRole('user');
 
-    //     $this->actingAs($user);
+        $this->actingAs($user);
 
-    //     // Crear un comentario utilizando el Factory
-    //     $comment = Comment::factory()->create([
-    //         'user_id' => $user->id,
-    //     ]);
-    //     $response = $this->post('api/comment/create/' . $comment->id, [
-    //         'score' => $comment->score,
-    //         'comment' => $comment->comment
-    //     ]);
+        // Crear un comentario utilizando el Factory
+        $comment = Comment::factory()->create([
+            'user_id' => $user->id,
+        ]);
+        $response = $this->post('api/comment/create/' . $comment->id, [
+            'score' => $comment->score,
+            'comment' => $comment->comment
+        ]);
 
-    //     $response->assertStatus(200);
-    // }
-
+        $response->assertStatus(200);
+    }
     /** @test */
-    // public function ok_is_returned_if_user_can_view_comment()
-    // {
-    //     // Crear un usuario utilizando el Factory
-    //       $user = User::factory()->create();
-    //       $user->assignRole('user');
+    public function ok_is_returned_if_user_can_view_comment()
+    {
+        // Crear un usuario utilizando el Factory
+          $user = User::factory()->create();
+          $user->assignRole('user');
         
-    //       // Loguearse con el usuario
-    //       $this->actingAs($user);
+          // Loguearse con el usuario
+          $this->actingAs($user);
 
-    //       // Crear un comentario utilizando el Factory
-    //        Comment::factory(3)->create([
-    //         'user_id' => $user->id,
-    //       ]);
+          // Crear un comentario utilizando el Factory
+           Comment::factory(3)->create([
+            'user_id' => $user->id,
+          ]);
     
-    //       // Verificar que devuelve la lista de comentarios del usuario
-    //       $response = $this->get('api/comments');
-    //       $response->assertStatus(200);
-    //       $response = $response->json();
-    //       $this->assertIsArray($response);
-    //       $commentsArray = $response['comments'];
-    //       $this->assertCount(3, $commentsArray);
-    // }
+          // Verificar que devuelve la lista de comentarios del usuario
+          $response = $this->get('api/comments');
+          $response->assertStatus(200);
+          $response = $response->json();
+          $this->assertIsArray($response);
+          $commentsArray = $response['comments'];
+          $this->assertCount(3, $commentsArray);
+    }
     /** @test */
-     //comment/update_my/{id}
-//     public function ok_is_returned_if_user_can_update_comment(){
-//         $user = User::factory()->create();
-//         $user->assignRole('user');
+    //  comment/update_my/{id}
+    public function ok_is_returned_if_user_can_update_comment(){
+        $user = User::factory()->create();
+        $user->assignRole('user');
 
-//         // Loguearse con el usuario
-//         $this->actingAs($user);
+        // Loguearse con el usuario
+        $this->actingAs($user);
       
-//         // Crear un comentario utilizando el Factory
-//         $comment = Comment::factory()->create([
-//           'user_id' => $user->id,
-//         ]);
+        // Crear un comentario utilizando el Factory
+        $comment = Comment::factory()->create([
+          'user_id' => $user->id,
+        ]);
     
-//         $response = $this->patch('api/comment/update_my/' . $comment->id, [
-//             'comment'=> 'nuevo comentario',
-//             'score'=> 5
-//         ]);
-//         $response->assertStatus(200);
-//   }
+        $response = $this->patch('api/comment/update_my/' . $comment->id, [
+            'comment'=> 'nuevo comentario',
+            'score'=> 5
+        ]);
+        $response->assertStatus(200);
+  }
     /** @test */
-    //comment/delete/index_my
-    // public function ok_is_returned_if_user_can_delete_comment(){
-    //       $user = User::factory()->create();
-    //       $user->assignRole('user');
+    // comment/delete/index_my
+    public function ok_is_returned_if_user_can_delete_comment(){
+          $user = User::factory()->create();
+          $user->assignRole('user');
 
-    //       // Loguearse con el usuario
-    //       $this->actingAs($user);
+          // Loguearse con el usuario
+          $this->actingAs($user);
         
-    //       // Crear un comentario utilizando el Factory
-    //       $comment = Comment::factory()->create([
-    //         'user_id' => $user->id,
-    //       ]);
+          // Crear un comentario utilizando el Factory
+          $comment = Comment::factory()->create([
+            'user_id' => $user->id,
+          ]);
       
-    //       $this->assertDatabaseHas('comments', [
-    //         'id' => $comment->id,
-    //       ]);
+          $this->assertDatabaseHas('comments', [
+            'id' => $comment->id,
+          ]);
       
-    //       $deletedComment = $this->delete('api/comment/delete_my/' . $comment->id);
-    //       $deletedComment->assertStatus(200);
+          $deletedComment = $this->delete('api/comment/delete_my/' . $comment->id);
+          $deletedComment->assertStatus(200);
       
-    //       $this->assertDatabaseMissing('comments', [
-    //           'id' => $comment->id,
-    //       ]);
-    // }
+          $this->assertDatabaseMissing('comments', [
+              'id' => $comment->id,
+          ]);
+    }
      /** @test */
-    //comment/delete/{id}
-//     public function ok_is_returned_if_superAdmin_can_delete_comment(){
-//         $superAdmin = User::factory()->create();
-//         $superAdmin->assignRole('superadmin');
+    // comment/delete/{id}
+    public function ok_is_returned_if_superAdmin_can_delete_comment(){
+        $superAdmin = User::factory()->create();
+        $superAdmin->assignRole('superadmin');
     
-//         $token = $superAdmin->createToken('Test Token')->accessToken;
-//         $headers = ['Authorization' => "Bearer $token"];
+        $token = $superAdmin->createToken('Test Token')->accessToken;
+        $headers = ['Authorization' => "Bearer $token"];
     
-//         // Loguearse con el usuario superadmin
-//         $this->actingAs($superAdmin);
-//         $this->assertAuthenticatedAs($superAdmin);
+        // Loguearse con el usuario superadmin
+        $this->actingAs($superAdmin);
+        $this->assertAuthenticatedAs($superAdmin);
     
-//         // Crear un comentario utilizando el Factory
-//         $comment = Comment::factory()->create([
-//           'user_id' => $superAdmin->id,
-//         ]);
+        // Crear un comentario utilizando el Factory
+        $comment = Comment::factory()->create([
+          'user_id' => $superAdmin->id,
+        ]);
     
-//         $this->assertDatabaseHas('comments', [
-//           'id' => $comment->id,
-//         ]);
+        $this->assertDatabaseHas('comments', [
+          'id' => $comment->id,
+        ]);
     
-//         $deletedComment = $this->delete('api/comment/delete/' . $comment->id, $headers);
-//         $deletedComment->assertStatus(200);
+        $deletedComment = $this->delete('api/comment/delete/' . $comment->id, $headers);
+        $deletedComment->assertStatus(200);
     
-//         $this->assertDatabaseMissing('comments', [
-//             'id' => $comment->id,
-//         ]);
-//   }
+        $this->assertDatabaseMissing('comments', [
+            'id' => $comment->id,
+        ]);
+  }
 
 }
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -1,288 +1,288 @@
 <?php
 
-// namespace Tests\Feature;
+namespace Tests\Feature;
 
-// use Tests\TestCase;
-// use App\Models\User;
-// use Illuminate\Support\Facades\Artisan;
-// use Spatie\Permission\Models\Role;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Artisan;
+use Spatie\Permission\Models\Role;
 
-// class UserTest extends TestCase
-// {
+class UserTest extends TestCase
+{
 
-//   public function setUp(): void {
-//     parent::setUp();
-//     Artisan::call('migrate:fresh --seed --env=testing');
+  public function setUp(): void {
+    parent::setUp();
+    Artisan::call('migrate:fresh --seed --env=testing');
 
-//     $this->post('api/register', [
-//       'username' => 'test user',
-//       'email' => 'testuser@example.com',
-//       'password' => '12345678',
-//       'phone' => '000111222'
-//     ]);
+    $this->post('api/register', [
+      'username' => 'test user',
+      'email' => 'testuser@example.com',
+      'password' => '12345678',
+      'phone' => '000111222'
+    ]);
 
-//     $this->post('api/register', [
-//       'username' => 'user admin',
-//       'email' => 'useradmin@example.com',
-//       'password' => '12345678',
-//       'phone' => '000222333'
-//     ]);
+    $this->post('api/register', [
+      'username' => 'user admin',
+      'email' => 'useradmin@example.com',
+      'password' => '12345678',
+      'phone' => '000222333'
+    ]);
 
-//     $adminRole = Role::where('name', 'admin')->first();
-//     $admin = User::where('username', 'user admin')->first();
+    $adminRole = Role::where('name', 'admin')->first();
+    $admin = User::where('username', 'user admin')->first();
 
-//     $admin->assignRole($adminRole);
+    $admin->assignRole($adminRole);
 
-//     $this->post('api/register', [
-//       'username' => 'user superadmin',
-//       'email' => 'usersuperadmin@example.com',
-//       'password' => '12345678',
-//       'phone' => '000333444'
-//     ]);
+    $this->post('api/register', [
+      'username' => 'user superadmin',
+      'email' => 'usersuperadmin@example.com',
+      'password' => '12345678',
+      'phone' => '000333444'
+    ]);
 
-//     $superadminRole = Role::where('name', 'superadmin')->first();
-//     $superadmin = User::where('username', 'user superadmin')->first();
+    $superadminRole = Role::where('name', 'superadmin')->first();
+    $superadmin = User::where('username', 'user superadmin')->first();
 
-//     $superadmin->assignRole($superadminRole);
-//   }
+    $superadmin->assignRole($superadminRole);
+  }
 
-  // //api/register
-  // /** @test*/
-  // public function ok_is_returned_if_user_user_register_are_completed(){
-  //   $response = $this->post('api/register', [
-  //     'username' => 'test user register',
-  //     'email' => 'testuserregister@example.com',
-  //     'password' => '12345678',
-  //     'phone' => '999111222'
-  //   ]);
-
-  //   $response->assertStatus(200);
-
-  //   $this->assertDatabaseHas('users', [
-  //     'email' => 'testuserregister@example.com',
-  //   ]);
-  // }
-
-  // /** @test*/
-  // public function ok_is_returned_if_user_admin_exists(){
-
-  //   $this->assertDatabaseHas('users', [
-  //     'email' => 'useradmin@example.com',
-  //   ]);
-  // }
-
-  // /** @test*/
-  // public function ok_is_returned_if_user_superadmin_exists(){
-
-  //   $this->assertDatabaseHas('users', [
-  //     'email' => 'usersuperadmin@example.com',
-  //   ]);
-
-  // }
-
-  // //api/login
-  // /** @test*/
-  // public function ok_is_returned_if_user_can_login(){
-  //   $response = $this->post('api/login', [
-  //     'email' => 'testuser@example.com',
-  //     'password' => '12345678'
-  //   ]);
-
-  //   $response->assertStatus(200);
-  // }
-
-  // /** @test*/
-  // public function ok_is_returned_if_user_can_refresh(){
-  //   $response = $this->post('api/login', [
-  //     'email' => 'testuser@example.com',
-  //     'password' => '12345678'
-  //   ]);
-
-  //   $token = $response->original['authorisation']['token'];
-
-  //   $response = $this->post('api/refresh', [
-  //     'token' => $token
-  //   ]);
-
-  //   $response->assertStatus(200);
-  // }
-
-  // //api/logout
-  // /** @test*/
-  // public function ok_is_returned_if_user_can_logout(){
-  //   $response = $this->post('api/login', [
-  //     'email' => 'testuser@example.com',
-  //     'password' => '12345678'
-  //   ]);
-
-  //   $token = $response->original['authorisation']['token'];
-
-  //   $response = $this->post('api/logout', [
-  //     'token' => $token
-  //   ]);
-
-  //   $response->assertStatus(200);
-  // }
-
-  // //api/me
-  // /** @test*/
-  // public function ok_is_returned_if_user_can_see_his_profile(){
-
-  //   $response = $this->post('api/login', [
-  //     'email' => 'testuser@example.com',
-  //     'password' => '12345678'
-  //   ]);
-
-  //   $token = $response->original['authorisation']['token'];
-
-  //   $response = $this->post('api/me', [
-  //     'token' => $token,
-  //   ]);
-
-  //   $response->assertStatus(200);
-  // }
-
-  // /** @test*/
-  // public function ok_is_returned_if_user_can_be_assigned_admin_role(){
-
-  //   $user = User::where('username', 'test user')->first();
-
-  //   $adminRole = Role::findByName('admin');
-
-  //   $user->assignRole($adminRole);
-
-  //   $this->assertTrue($user->hasRole('admin'));
-  // }
-
-  // /** @test*/
-  // public function ok_is_returned_if_user_can_be_assigned_superadmin_role(){
-
-  //   $user = User::where('username', 'test user')->first();
-
-  //   $superadminRole = Role::findByName('superadmin');
-
-  //   $user->assignRole($superadminRole);
-
-  //   $this->assertTrue($user->hasRole('superadmin'));
-  // }
-
-  // //api/me/update
-  // /** @test*/
-  // public function ok_is_returned_if_user_can_update_his_profile(){
-
-  //   $response = $this->post('api/login', [
-  //     'email' => 'testuser@example.com',
-  //     'password' => '12345678'
-  //   ]);
-
-  //   $token = $response->original['authorisation']['token'];
-
-  //   $response = $this->patch('api/me/update', [
-  //     'username' => 'test user updated',
-  //     'email' => 'testuserupdated@example.com',
-  //     'password' => '12345678',
-  //     'phone' => '678678678',
-  //     'token' => $token
-  //   ]);
-
-  //   $response->assertStatus(200);
-
-  // }
-
-  // //api/me/delete
-  // /** @test*/
-  // public function ok_is_returned_if_user_can_delete_his_profile(){
-  //   $response = $this->post('api/login', [
-  //     'email' => 'testuser@example.com',
-  //     'password' => '12345678'
-  //   ]);
-
-  //   $token = $response->original['authorisation']['token'];
-
-  //   $response = $this->delete('api/me/delete', [
-  //     'token' => $token
-  //   ]);
-
-  //   $response->assertStatus(200);
-  // }
-
-  // //api/user/delete/{id}
-  // /** @test*/
-  // public function ok_is_returned_if_user_superadmin_can_delete_user_admin_profile(){
-  //   $response = $this->post('api/login', [
-  //     'email' => 'usersuperadmin@example.com',
-  //     'password' => '12345678'
-  //   ]);
-
-  //   // Verifica que el código de respuesta HTTP sea 200
-  //   $response->assertStatus(200);
-
-  //   $token = $response->original['authorisation']['token'];
-  //   $userToDelete = User::where('username', 'admin')->first();
-  //   $userIdToDelete = $userToDelete->id;
-
-  //   // Crea una nueva petición POST a la ruta de eliminación de usuario utilizando el token de acceso
-
-  //   $response = $this->delete('api/user/delete/' . $userIdToDelete, [
-  //     'Bearer' => $token,
-  //   ]);
-
-  //   // Verifica que el código de respuesta HTTP sea 200
-  //   $response->assertStatus(200);
-
-  //   // Verifica que el usuario haya sido eliminado de la base de datos
-  //   $this->assertDatabaseMissing('users', [
-  //       'id' => $userIdToDelete,
-  //   ]);
-  // }
-
-  // //api/me/delete
+  //api/register
   /** @test*/
-  // public function ok_is_returned_if_user_superadmin_can_delete_his_profile(){
-  //   $response = $this->post('api/login', [
-  //     'email' => 'usersuperadmin@example.com',
-  //     'password' => '12345678'
-  //   ]);
+  public function ok_is_returned_if_user_user_register_are_completed(){
+    $response = $this->post('api/register', [
+      'username' => 'test user register',
+      'email' => 'testuserregister@example.com',
+      'password' => '12345678',
+      'phone' => '999111222'
+    ]);
 
-  //   // $token = $response->original['authorisation']['token'];
+    $response->assertStatus(200);
 
-  //   $response = $this->delete('api/me/delete', [
-  //     // 'token' => $token
-  //   ]);
+    $this->assertDatabaseHas('users', [
+      'email' => 'testuserregister@example.com',
+    ]);
+  }
 
-  //   $response->assertStatus(200);
-  // }
-
-  //api/user/update/{id}
   /** @test*/
-  // public function ok_is_returned_if_user_superadmin_can_update_user_role(){
-  //   $response = $this->post('api/login', [
-  //     'email' => 'usersuperadmin@example.com',
-  //     'password' => '12345678'
-  //   ]);
+  public function ok_is_returned_if_user_admin_exists(){
 
-  //   $response->assertStatus(200);
+    $this->assertDatabaseHas('users', [
+      'email' => 'useradmin@example.com',
+    ]);
+  }
 
-  //   // $token = $response->original['authorisation']['token'];
-  //   $userToUpgrade = User::where('email', 'testuser@example.com')->first();
-  //   $userToUpgradeId = $userToUpgrade->id;
+  /** @test*/
+  public function ok_is_returned_if_user_superadmin_exists(){
 
-  //   $response = $this->patch('api/user/update/' . $userToUpgradeId, [
-  //     // 'Bearer' => $token,
-  //     'role' => "admin"
-  //   ]);
+    $this->assertDatabaseHas('users', [
+      'email' => 'usersuperadmin@example.com',
+    ]);
 
-  //   $response->assertStatus(200);
+  }
 
-  //   $response = $this->post('api/login', [
-  //     'email' => 'testuser@example.com',
-  //     'password' => '12345678'
-  //   ]);
+  //api/login
+  /** @test*/
+  public function ok_is_returned_if_user_can_login(){
+    $response = $this->post('api/login', [
+      'email' => 'testuser@example.com',
+      'password' => '12345678'
+    ]);
 
-  //   $token = $response->original['authorisation']['token'];
+    $response->assertStatus(200);
+  }
 
-  //   $response = $this->post('api/me', [
-  //     'token' => $token,
-  //   ]);
-  // }
-// }
+  /** @test*/
+  public function ok_is_returned_if_user_can_refresh(){
+    $response = $this->post('api/login', [
+      'email' => 'testuser@example.com',
+      'password' => '12345678'
+    ]);
+
+    $token = $response->original['authorisation']['token'];
+
+    $response = $this->post('api/refresh', [
+      'token' => $token
+    ]);
+
+    $response->assertStatus(200);
+  }
+
+  //api/logout
+  /** @test*/
+  public function ok_is_returned_if_user_can_logout(){
+    $response = $this->post('api/login', [
+      'email' => 'testuser@example.com',
+      'password' => '12345678'
+    ]);
+
+    $token = $response->original['authorisation']['token'];
+
+    $response = $this->post('api/logout', [
+      'token' => $token
+    ]);
+
+    $response->assertStatus(200);
+  }
+
+  //api/me
+  /** @test*/
+  public function ok_is_returned_if_user_can_see_his_profile(){
+
+    $response = $this->post('api/login', [
+      'email' => 'testuser@example.com',
+      'password' => '12345678'
+    ]);
+
+    $token = $response->original['authorisation']['token'];
+
+    $response = $this->post('api/me', [
+      'token' => $token,
+    ]);
+
+    $response->assertStatus(200);
+  }
+
+  /** @test*/
+  public function ok_is_returned_if_user_can_be_assigned_admin_role(){
+
+    $user = User::where('username', 'test user')->first();
+
+    $adminRole = Role::findByName('admin');
+
+    $user->assignRole($adminRole);
+
+    $this->assertTrue($user->hasRole('admin'));
+  }
+
+  /** @test*/
+  public function ok_is_returned_if_user_can_be_assigned_superadmin_role(){
+
+    $user = User::where('username', 'test user')->first();
+
+    $superadminRole = Role::findByName('superadmin');
+
+    $user->assignRole($superadminRole);
+
+    $this->assertTrue($user->hasRole('superadmin'));
+  }
+
+  //api/me/update
+  /** @test*/
+  public function ok_is_returned_if_user_can_update_his_profile(){
+
+    $response = $this->post('api/login', [
+      'email' => 'testuser@example.com',
+      'password' => '12345678'
+    ]);
+
+    $token = $response->original['authorisation']['token'];
+
+    $response = $this->patch('api/me/update', [
+      'username' => 'test user updated',
+      'email' => 'testuserupdated@example.com',
+      'password' => '12345678',
+      'phone' => '678678678',
+      'token' => $token
+    ]);
+
+    $response->assertStatus(200);
+
+  }
+
+  //api/me/delete
+  /** @test*/
+  public function ok_is_returned_if_user_can_delete_his_profile(){
+    $response = $this->post('api/login', [
+      'email' => 'testuser@example.com',
+      'password' => '12345678'
+    ]);
+
+    $token = $response->original['authorisation']['token'];
+
+    $response = $this->delete('api/me/delete', [
+      'token' => $token
+    ]);
+
+    $response->assertStatus(200);
+  }
+
+  //api/user/delete/{id}
+  /** @test*/
+  public function ok_is_returned_if_user_superadmin_can_delete_user_admin_profile(){
+    $response = $this->post('api/login', [
+      'email' => 'usersuperadmin@example.com',
+      'password' => '12345678'
+    ]);
+
+    // Verifica que el código de respuesta HTTP sea 200
+    $response->assertStatus(200);
+
+    $token = $response->original['authorisation']['token'];
+    $userToDelete = User::where('username', 'admin')->first();
+    $userIdToDelete = $userToDelete->id;
+
+    // Crea una nueva petición POST a la ruta de eliminación de usuario utilizando el token de acceso
+
+    $response = $this->delete('api/user/delete/' . $userIdToDelete, [
+      'Bearer' => $token,
+    ]);
+
+    // Verifica que el código de respuesta HTTP sea 200
+    $response->assertStatus(200);
+
+    // Verifica que el usuario haya sido eliminado de la base de datos
+    $this->assertDatabaseMissing('users', [
+        'id' => $userIdToDelete,
+    ]);
+  }
+
+  //api/me/delete
+  /** @test*/
+  public function ok_is_returned_if_user_superadmin_can_delete_his_profile(){
+    $response = $this->post('api/login', [
+      'email' => 'usersuperadmin@example.com',
+      'password' => '12345678'
+    ]);
+
+    // $token = $response->original['authorisation']['token'];
+
+    $response = $this->delete('api/me/delete', [
+      // 'token' => $token
+    ]);
+
+    $response->assertStatus(200);
+  }
+
+  // api/user/update/{id}
+  /** @test*/
+  public function ok_is_returned_if_user_superadmin_can_update_user_role(){
+    $response = $this->post('api/login', [
+      'email' => 'usersuperadmin@example.com',
+      'password' => '12345678'
+    ]);
+
+    $response->assertStatus(200);
+
+    // $token = $response->original['authorisation']['token'];
+    $userToUpgrade = User::where('email', 'testuser@example.com')->first();
+    $userToUpgradeId = $userToUpgrade->id;
+
+    $response = $this->patch('api/user/update/' . $userToUpgradeId, [
+      // 'Bearer' => $token,
+      'role' => "admin"
+    ]);
+
+    $response->assertStatus(200);
+
+    $response = $this->post('api/login', [
+      'email' => 'testuser@example.com',
+      'password' => '12345678'
+    ]);
+
+    $token = $response->original['authorisation']['token'];
+
+    $response = $this->post('api/me', [
+      'token' => $token,
+    ]);
+  }
+}
